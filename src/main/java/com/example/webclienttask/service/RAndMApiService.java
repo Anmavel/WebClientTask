ed636d468e2ce6509026afbb29db482ee6257d9f
Aniii <Ana@example.com>
2023-02-09 12:55:44 +0100

    Changes by valentinfinke

--- src/main/java/com/example/webclienttask/service/RAndMApiService.java
+++ src/main/java/com/example/webclienttask/service/RAndMApiService.java
@@ -2,11 +2,13 @@ package com.example.webclienttask.service;
 
 import com.example.webclienttask.model.RAndMApiResponse;
 import com.example.webclienttask.model.RAndMCharacter;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Objects;
 
+@Service
 public class RAndMApiService {
 
     private WebClient webClient = WebClient.create("https://rickandmortyapi.com/api");
@@ -17,6 +19,20 @@ public class RAndMApiService {
                         .uri("/character")
                         .retrieve()
                         .toEntity(RAndMApiResponse.class)
+                        .block())
+                .getBody();
+        return rAndMApiResponse.results();
+
+    }
+
+    public List<RAndMCharacter> getLivingCharacters() {
+        RAndMApiResponse rAndMApiResponse = Objects.requireNonNull(webClient
+                        .get()
+                        .uri(uriBuilder -> uriBuilder
+                                        .path("/character")
+                                        .queryParam("status", "Alive")
+                                        .build())
+                        .retrieve().toEntity(RAndMApiResponse.class)
                         .block())
                 .getBody();
         return rAndMApiResponse.results();
